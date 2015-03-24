63de7e3d553bd51b345c120945b15ad5d6e1e4b7
Adam Malantonio <adam.malantonio@gmail.com>
2015-03-23 22:45:08 -0400

    Dewey comments + better exceptions

--- src/Dewey.php
+++ src/Dewey.php
@@ -15,6 +15,9 @@ class Dewey {
      *      ex. "74x.22" will return ["740.22", "750.22"]
      *
      *  and will _not_ work w/ cutters, as our placeholder (`x`) may be used w/in a cutter
+     *
+     *  @param  string  call number to range, using X to denote where the range is (see above)
+     *  @return array   tuple array -> array($minNumber, $maxNumber)
      */
 
     public static function calculateRange($rangeStr) {
@@ -65,6 +68,16 @@ class Dewey {
         return array($min, $max);
     }
 
+    /**
+     *  compares two DDS call numbers (including cutters) using the provided operator
+     *
+     *  @param  mixed   base DDS call number, can be string or Dewey\CallNumber object
+     *  @param  mixed   DDS call number being compared against base, can be string or Dewey\CallNumber object
+     *  @param  string  operator to perform comparison (follows form: $input $operator $comp)
+     *  @return bool
+     *  @throws InvalidArgumentException
+     */
+
     public static function compare($input, $comp, $operator) {
         if ( !is_a($input, "Dewey\CallNumber") ) {
             $input = self::parseCallNumber($input);
@@ -97,15 +110,23 @@ class Dewey {
 
             // unneccessary but available
             case "===": return $inputNormalized === $compNormalized;
-            default: throw new Exception("Unrecognized operator: [{$operator}]");
+            default: throw new \InvalidArgumentException("Invalid operator: [{$operator}]");
         }
     }
+
+    /**
+     *  parses a Dewey\CallNumber object from a DDS call number string
+     *
+     *  @param  string              DDS call number input
+     *  @return Dewey\CallNumber
+     *  @throws InvalidArgumentException
+     */
 
     public static function parseCallNumber($ddString) {
         preg_match(self::DDS_FULL_REGEX, $ddString, $matches);
 
         // handle bad Call Number
-        if ( empty($matches) ) { throw new Exception("Malformed Dewey Decimal call number"); }
+        if ( empty($matches) ) { throw new \InvalidArgumentException("Malformed Dewey Decimal call number"); }
 
         $major = $matches[1];
         $minor = $matches[2];
