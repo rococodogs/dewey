e7ad0b8c7b9e5cddc5eeeaf5f781220701324e81
Adam Malantonio <adam.malantonio@gmail.com>
2015-03-23 22:15:29 -0400

    testin'

--- test/CallNumberTest.php
+++ test/CallNumberTest.php
@@ -6,11 +6,11 @@ class CallNumberTest extends PHPUnit_Framework_TestCase {
     protected $callNumber = "514.123";
     protected $cutter = "A987x";
 
-    protected $normalized =         "5141230198724";
+    protected $normalized           = "5141230198724";
     protected $normalizedLessDigits = "5141000198724";
-    protected $normalizedCNPadded = "514123000198724";
-    protected $normalizedCTPadded = "51412301987240";
-    protected $normalizedPadded   = "514123000198724000";
+    protected $normalizedCNPadded   = "514123000198724";
+    protected $normalizedCTPadded   = "51412301987240";
+    protected $normalizedPadded     = "5141230001987240";
 
     public function setUp() {
         $this->cn = new Dewey\CallNumber;
@@ -19,7 +19,44 @@ class CallNumberTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testToString() {
-        $this->assertEquals($this->callNumberString, $this->cn . '');
+        $this->assertEquals(
+            $this->callNumberString, 
+            $this->cn . '',
+            'CallNumber prints as a readable DDS call number'
+        );
+    }
+
+    public function testCalculateNormalizedCutter() {
+        $ct = "0198724";
+        
+        $this->assertEquals(
+            $ct, 
+            $this->cn->calculateNormalizedCutter(),
+            'no padding'
+        );
+        
+        $this->assertEquals(
+            $ct, 
+            $this->cn->calculateNormalizedCutter($this->cn->getNormalizedCutterLength() - 1),
+            'slight pading'
+        );
+
+        $this->assertEquals(
+            $ct . "00", 
+            $this->cn->calculateNormalizedCutter($this->cn->getNormalizedCutterLength() + 2), 
+            'noticeable padding'
+        );
+    }
+
+    public function testCompare() {
+        $this->assertTrue(
+            $this->cn->compare($this->callNumberString, "=="),
+            'Double equals should compare true'
+        );
+    }
+
+    public function testEqualTo() {
+        $this->assertTrue($this->cn->equalTo($this->cn));
     }
 
     public function testGetCallNumberLength() {
@@ -27,7 +64,71 @@ class CallNumberTest extends PHPUnit_Framework_TestCase {
     }
 
     public function testGetCutterLength() {
-        $this->assertEquals(5, $this->cn->getCutterLength());
+        $this->assertEquals(
+            5, 
+            $this->cn->getCutterLength(),
+            'Getting the cutter length should return not normalized length'
+        );
+
+        $this->assertEquals(
+            7,
+            $this->cn->getCutterLength(true),
+            'Passing `true` to getCutterLength will calculate normalized cutter length'
+        );
+    }
+
+    public function testGreaterThan() {
+        $this->assertTrue(
+            $this->cn->greaterThan($this->callNumber),
+            'A DDS call number w/ a cutter will calculate greater than the same w/o'
+        );
+    }
+
+    public function testGreaterThanEqualTo() {
+        $this->assertTrue(
+            $this->cn->greaterThan($this->callNumber),
+            'A DDS call number w/ a cutter will calculate greater than the same w/o'
+        );
+
+        $this->assertTrue(
+            $this->cn->greaterThanEqualTo($this->cn),
+            'greaterThanEqualTo should equate true for equal to'
+        );
+    }
+
+    public function testInRange() {
+        $this->assertTrue($this->cn->inRange(array("510", "520")));
+        $this->assertTrue($this->cn->inRange("514", "515"));
+    }
+
+    public function testLessThan() {
+        $newCN = \Dewey::parseCallNumber($this->callNumber);
+        $this->assertTrue(
+            $newCN->lessThan($this->cn),
+            'A DDS call number w/o a cutter will calculate less than the same with'
+        );
+
+        $this->assertTrue(
+            $this->cn->lessThan("515"),
+            'lessThan works with a single major number'
+        );
+        
+        $this->assertFalse(
+            $this->cn->lessThan($newCN),
+            'instance CallNumber shouldn\'t be less than the test one'
+        );
+    }
+
+    public function testLessThanEqualTo() {
+        $this->assertTrue(
+            $this->cn->lessThan("520"),
+            'lessThanEqualTo a number larger than self should return true'
+        );
+        
+        $this->assertTrue(
+            $this->cn->lessThanEqualTo($this->cn),
+            'lessThanEqualTo self should return true'
+        );
     }
 
     public function testNormalized() {
@@ -50,7 +151,7 @@ class CallNumberTest extends PHPUnit_Framework_TestCase {
     public function testNormalizedCutterPadding() {        
         $this->assertEquals(
             $this->normalizedCTPadded,
-            $this->cn->calculateNormalized(array('cutter' => 6))
+            $this->cn->calculateNormalized(array('cutter' => 8))
         );
     }
 
--- test/DeweyTest.php
+++ test/DeweyTest.php
@@ -3,14 +3,46 @@ class DeweyTest extends PHPUnit_Framework_TestCase {
 
     protected $callNumber = "514.123 A997x";
 
+    public function testCalculateRange() {
+        $this->assertEquals(
+            array("740", "750"),
+            Dewey::calculateRange("74x"),
+            'Range works w/ ones-place X'
+        );
+
+        $this->assertEquals(
+            array("700", "800"),
+            Dewey::calculateRange("7xx"),
+            'Range works w/ tens-place + ones-place Xs'
+        );
+    }
+
+    public function testCalculateRangeDecimal() {
+        $this->assertEquals(
+            array("740.0", "741.0"),
+            Dewey::calculateRange("740.x"),
+            'Range affects ones-place when using single X'
+        );
+
+        $this->assertEquals(
+            array("740.20", "740.30"),
+            Dewey::calculateRange("740.2x"),
+            'Range drills down to tenths'
+        );
+    }
+
     public function testCompareEQEQ() {
         $this->assertTrue(Dewey::compare($this->callNumber, $this->callNumber, "=="));
+    }
+
+    public function testCompareEQEQEQ() {
+        $this->assertTrue(Dewey::compare($this->callNumber, $this->callNumber, "==="));
     }
 
     public function testCompareGT() {
         $this->assertTrue(Dewey::compare($this->callNumber, "510", ">"));
         $this->assertTrue(Dewey::compare($this->callNumber, "514.12 A997w", ">"));
-        $this->assertFalse(Dewey::compare($this->callNumber, "610.123 L019l", ">"));
+        $this->assertFalse(Dewey::compare($this->callNumber, "514.1230 A997x", ">"));
         $this->assertFalse(Dewey::compare($this->callNumber, $this->callNumber, ">"));
     }
 
@@ -25,6 +57,7 @@ class DeweyTest extends PHPUnit_Framework_TestCase {
     }
     public function testCompareLTEQ() {
         $this->assertTrue(Dewey::compare($this->callNumber, "514.123 A998a", "<="));
+        $this->assertTrue(Dewey::compare($this->callNumber, "520", "<="));
         $this->assertTrue(Dewey::compare($this->callNumber, $this->callNumber, "<="));
     }
 }
